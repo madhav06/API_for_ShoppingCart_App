0028ae9698c1ef09fb10243d4136aa7a3deafc44
madhav06 <mnandan06@gmail.com>
2021-08-15 03:17:28 +0530

    GET Request, adding endpoints

---
+++ shopping_cart/api_app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+import json
+from .models import CartItem
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+class ShoppingCart(View):
+    def post(self, request):
+
+        data = json.loads(request.body.decode("utf-8"))
+        p_name = data.get('product_name')
+        p_price = data.get('product_price')
+        p_quantity = data.get('product_quantity')
+
+        product_data = {
+            'product_name': p_name,
+            'product_price': p_price,
+            'product_quantity': p_quantity,
+        }
+
+        cart_item = CartItem.objects.create(**product_data)
+
+        data = {
+            "message": f"New item added to cart with id: {cart_item.id}"
+        }
+        return JsonResponse(data, status=201)
+
+    def get(self, request):
+        items_count = CartItem.objects.count()
+        items = CartItem.objects.all()
+
+        items_data = []
+        for item in items:
+            items_data.append({
+                'product_name': item.product_name,
+                'product_price': item.product_price,
+                'product_quantity': item.product_quantity,
+            })
+        data = {
+            'items': items_data,
+            'count': items_count,
+        }
+        return JsonResponse(data)
+
